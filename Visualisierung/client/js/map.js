a0a90c30dd428e1047bebe62b82ff4971c08d93c
Florian Hrycaj <hrycajf@gmail.com>
2018-01-14 12:48:16 +0100

    Added maps

--- Visualisierung/client/js/map.js
+++ Visualisierung/client/js/map.js
@@ -12,11 +12,31 @@ var map = L.map('map', {zoomControl: false }).setView([koelnlat, koelnlng], zoom
 map.scrollWheelZoom.disable();
 map.dragging.disable();
 
-var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; ' + mapLink + ' Contributors',
+// Original openstreetmap map
+// var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+// L.tileLayer(
+//     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; ' + mapLink + ' Contributors',
+// }).addTo(map);
+
+// Dark themed map with labels
+// L.tileLayer(
+//     'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+// }).addTo(map);
+
+
+// Dark themed map without labels, white street lines
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 }).addTo(map);
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lines/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	ext: 'png'
+}).addTo(map);
+//
+// var accessToken = "pk.eyJ1IjoiaHJ5Y2FqZiIsImEiOiJjamE0aWRienM5ejMxMzNsZ2ZwZG9wZ3Q4In0._r3j2dQt_yXrM9dFZxzv5Q";
+
 
 var svgLayer = L.svg();
 svgLayer.addTo(map);
