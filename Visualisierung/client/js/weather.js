5cb08117d8a89146ac141ec01cfa947277f32759
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-01-15 01:00:54 +0100

    Time

--- Visualisierung/client/js/weather.js
+++ Visualisierung/client/js/weather.js
@@ -1,9 +1,9 @@
 
 function drawWeather(callback){
 
-  console.log("HALLOOOOO");
-
+  console.log(bikes[0].b_time);
   var weatherText = bikes[0].w_text;
+  var time = new Date(bikes[0].b_time);
   var temp = parseFloat(bikes[0].temp);
   temp = temp.toFixed(1);
   var imagePath = "./images/" + weatherText + ".svg";
@@ -12,7 +12,7 @@ function drawWeather(callback){
   console.log(imagePath);
 
   async.series([
-      function(callback) {drawImages(callback, imagePath, temp);}
+      function(callback) {drawImages(callback, imagePath, temp, time);}
   ], function(err) {
       if (err) {
           console.log(err);
@@ -23,7 +23,7 @@ function drawWeather(callback){
 }
 
 
-function drawImages(callback, imagePath, temp){
+function drawImages(callback, imagePath, temp, time){
 
   var svg = d3.select(".weather").append("svg")
       .attr("width", 500)
@@ -56,6 +56,16 @@ function drawImages(callback, imagePath, temp){
       .attr("height", 50)
       .attr("x", 210)
       .attr("y", 25);
+
+  var time2 = g.append("text")
+      .html(time)
+      .attr("font-size", 25)
+      .attr("width", 200)
+      .attr("height", 200)
+      .style('fill', 'white')
+      .attr('class', 'weather-text')
+      .attr("x", 15)
+      .attr("y", 130);
 
     callback();
 }
